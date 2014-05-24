9ba91551053bfc01eb5e26ce6a7bf11b5e9b7a81
wem-wem <spidertargal@yahoo.co.jp>
2014-05-24 17:49:30 +0900

    改訂版

--- Wem/hellox10.c
+++ Wem/hellox10.c
@@ -2,9 +2,9 @@
 
 int main()
 {
-	int i;
-	for (i = 0; i < 10; i++){
-			printf("Hello World\n");
-	}
-	return 0;
+    int i;
+    for (i = 0; i < 10; i++){
+    printf("Hello World\n");
+    }
+    return 0;
 }
